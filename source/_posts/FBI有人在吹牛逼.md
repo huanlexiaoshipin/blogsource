826b08b507f8cb1572b24b3a1e1f605d7a62b4db
test <test@qq.com>
2016-12-09 10:56:44 +0800

    add

---
+++ source/_posts/FBI有人在吹牛逼.md
@@ -0,0 +1,9 @@
+
+---
+title: "FBI有人在吹牛逼"
+date: 2016-12-09-00-47-39
+tags: [搞笑,谐音]
+category: "搞笑"
+description: "FBI有人在吹牛逼"
+---
+<video src="http://ohtsqip0g.bkt.clouddn.com/FBI有人在吹牛逼-搞笑,谐音-搞笑.mp4" controls="controls"></video>
--- themes/hueman/scripts/excerpt.js
+++ themes/hueman/scripts/excerpt.js
@@ -5,11 +5,5 @@
 *     <%- excerpt(post) %>
 */
 hexo.extend.helper.register('excerpt', function (post) {
-    var excerpt;
-    if (post.excerpt) {
-        excerpt = post.excerpt.replace(/\<[^\>]+\>/g, '');
-    } else {
-        excerpt = post.content.replace(/\<[^\>]+\>/g, '').substring(0, 200);
-    }
-    return excerpt;
+    return post.content;
 });
